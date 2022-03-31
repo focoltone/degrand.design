02268b1c944e4fa005769a83a0c1b0f02d1b2167
Colton DeGrand <cpdegrand@gmail.com>
2022-03-31 12:06:05 -0500

    add pw

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,15 @@
 //https://www.movable-type.co.uk/scripts/sha1.html
 var expirationDays = 3;
 var users = [
-  '35c6655c3f459a3932fc59fc059d2b241105dc15', 'ab0b2d3131715348e93e811998226e46a5adbda3', 'b5e06cc181f17e1859aec2d4c9ad0867479e39d6',
-];
+  '35c6655c3f459a3932fc59fc059d2b241105dc15', 
+  'ab0b2d3131715348e93e811998226e46a5adbda3', 
+  'b5e06cc181f17e1859aec2d4c9ad0867479e39d6', 
+  'f28340c94aa1d5568867c374a12aa7d86f778a7f',
+  '7968979b63f30aff931148c94aa09a11615d5388',
+  '3d22df945c183b681ef6dd154afe50d09e8c0a6d',
+
+
+  ];
 
 function setCookie(user, value, days) {
   const date = new Date();
