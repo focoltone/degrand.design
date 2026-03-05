3e1a8b9aedac32ba93a11b83cc8bfc74ee4d1661
Colton <focoltone@users.noreply.github.com>
2026-03-04 22:36:18 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,45 +1,45 @@
 //https://www.movable-type.co.uk/scripts/sha1.html
-var expirationDays = 200;
-var users = [
- '6ab9a07ee86796612f9b1dce15d83d153bf60ba3'
-  ];
+// var expirationDays = 200;
+// var users = [
+//  '6ab9a07ee86796612f9b1dce15d83d153bf60ba3'
+//   ];
 
-function setCookie(user, value, days) {
-  const date = new Date();
-  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-  var expires = "expires=" + date.toUTCString();  
-  document.cookie = user + "=" + value + ";" + expires + ";path=/";
-}
+// function setCookie(user, value, days) {
+//   const date = new Date();
+//   date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+//   var expires = "expires=" + date.toUTCString();  
+//   document.cookie = user + "=" + value + ";" + expires + ";path=/";
+// }
 
-function getCookie(user) {
-  let name = user + "=";
-  let spli = document.cookie.split(';');
-  for(var j = 0; j < spli.length; j++) {
-    let char = spli[j];
-    while (char.charAt(0) == ' ') {
-      char = char.substring(1);
-    }
-    if (char.indexOf(name) == 0) {
-      return char.substring(name.length, char.length);
-    }
-  }  
-}
+// function getCookie(user) {
+//   let name = user + "=";
+//   let spli = document.cookie.split(';');
+//   for(var j = 0; j < spli.length; j++) {
+//     let char = spli[j];
+//     while (char.charAt(0) == ' ') {
+//       char = char.substring(1);
+//     }
+//     if (char.indexOf(name) == 0) {
+//       return char.substring(name.length, char.length);
+//     }
+//   }  
+// }
 
-function checkCookie() {
-  var user = getCookie("user");
-  if(!user) {
-    window.location = '/login.html';
-  } 
-}
+// function checkCookie() {
+//   var user = getCookie("user");
+//   if(!user) {
+//     window.location = '/login.html';
+//   } 
+// }
 
-function login () {
-  while(!getCookie("user")) {
-    var user = prompt("Please enter your password:", "");
-    //set cookie
-    if (user != "" && user != null && users.includes(Sha1.hash(user))) {
-      setCookie("user", Sha1.hash(user), expirationDays);
-    }
-  }
+// function login () {
+//   while(!getCookie("user")) {
+//     var user = prompt("Please enter your password:", "");
+//     //set cookie
+//     if (user != "" && user != null && users.includes(Sha1.hash(user))) {
+//       setCookie("user", Sha1.hash(user), expirationDays);
+//     }
+//   }
   window.location = '/';
 }
 
